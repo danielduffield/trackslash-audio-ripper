7a9c02f4b67ac77280c66c2fd5239abcb0b083ae
Daniel Duffield <danieljduffield@gmail.com>
2017-07-12 12:23:24 -0700

    Fixed a bug where timecodes were not properly extracted from the description. #2

--- processMetadata.js
+++ processMetadata.js
@@ -2,26 +2,14 @@ const moment = require('moment')
 
 function processMetadata(data) {
   const keyData = {}
-  const timeCodeLines = findTimeCodes(data.description)
+  const timeCodeRows = findTimeCodes(data.description)
   keyData.videoTitle = data.title
   keyData.videoId = data.video_id
   keyData.videoImage = 'https://img.youtube.com/vi/' + data.video_id + '/maxresdefault.jpg'
-  keyData.timeCodes = extractTimeCodes(timeCodeLines)
+  keyData.timeCodes = extractTimeCodes(timeCodeRows)
   keyData.videoLength = secondsToHours(data.length_seconds)
   keyData.description = data.description
   return keyData
-}
-
-function extractTimeCodes(parsedDescription) {
-  const timeCodes = []
-  parsedDescription.forEach(row => {
-    row.forEach(string => {
-      if (string.includes(':')) {
-        timeCodes.push(string)
-      }
-    })
-  })
-  return timeCodes
 }
 
 function secondsToHours(seconds) {
@@ -38,18 +26,27 @@ module.exports = processMetadata
 function findTimeCodes(description) {
   const newlineSplit = description.split('\n')
   const startIndex = newlineSplit.findIndex(row => {
-    return (row.includes(' - ') && row.includes(':'))
+    return (row.includes(':') && !row.includes('Tracks') && !row.includes('tracks'))
   })
   const reversed = newlineSplit.slice()
   reversed.reverse()
   const lastLine = reversed.findIndex(row => {
-    return (row.includes(' - ') && row.includes(':'))
+    return (row.includes(':') && !row.includes('Tracks') && !row.includes('tracks'))
   })
   const endIndex = (newlineSplit.length - 1) - lastLine
-  const timestampedRows = newlineSplit.splice(startIndex, (endIndex - startIndex))
-  const parsed = []
-  timestampedRows.forEach(row => {
-    parsed.push(row.split(' '))
+  const timeStampedRows = newlineSplit.splice(startIndex, (endIndex - startIndex))
+  return timeStampedRows
+}
+
+function extractTimeCodes(rowsWithCodes) {
+  const timeCodes = []
+  rowsWithCodes.forEach(row => {
+    const spaceSplit = row.split(' ')
+    spaceSplit.forEach(string => {
+      if (string.includes(':')) {
+        timeCodes.push(string)
+      }
+    })
   })
-  return parsed
+  return timeCodes
 }
