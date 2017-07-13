57c49e13d2706a46e07a033971b4c5e79b734911
Daniel Duffield <danieljduffield@gmail.com>
2017-07-12 23:48:56 -0700

    Added videoLengthString as a property of the keyData object. #2

--- processMetadata.js
+++ processMetadata.js
@@ -7,18 +7,35 @@ function processMetadata(data) {
   keyData.videoId = data.video_id
   keyData.videoImage = 'https://img.youtube.com/vi/' + data.video_id + '/maxresdefault.jpg'
   keyData.timeCodes = extractTimeCodes(timeCodeRows)
-  keyData.videoLength = secondsToHours(data.length_seconds)
+  keyData.videoLength = secondsToTimeObject(data.length_seconds)
+  keyData.videoLengthString = secondsToTimeString(data.length_seconds)
   keyData.description = data.description
   return keyData
 }
 
-function secondsToHours(seconds) {
+function secondsToTimeObject(seconds) {
   const tempTime = moment.duration(seconds * 1000)
   const time = {}
   time.hours = tempTime.hours()
   time.minutes = tempTime.minutes()
   time.seconds = tempTime.seconds()
   return time
+}
+
+function secondsToTimeString(seconds) {
+  const tempTime = moment.duration(seconds * 1000)
+  const time = {}
+  time.hours = tempTime.hours()
+  time.minutes = tempTime.minutes()
+  time.seconds = tempTime.seconds()
+  if (time.minutes < 10) {
+    time.minutes = '0' + time.minutes.toString()
+  }
+  if (time.seconds < 10) {
+    time.seconds = '0' + time.seconds.toString()
+  }
+  const timeString = time.hours + ':' + time.minutes + ':' + time.seconds
+  return timeString
 }
 
 module.exports = processMetadata
