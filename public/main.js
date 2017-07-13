8a5be80fc2f997298818018293eb49b249d49ad6
Daniel Duffield <danieljduffield@gmail.com>
2017-07-12 21:30:32 -0700

    Added to the createFormTable function to include row element and col div. #2

--- public/main.js
+++ public/main.js
@@ -107,19 +107,24 @@ function submitTracks(form, numOfTracks) {
 }
 
 function createFormTable() {
-  const $formTable = createElement('form', {id: 'tracklist-form'}, '', [
-    createElement('table', {class: 'table table-bordered'}, '', [
-      createElement('thead', {}, '', [
-        createElement('tr', {}, '', [
-          createElement('th', {}, 'Num', []),
-          createElement('th', {}, 'Name', []),
-          createElement('th', {}, 'Start', []),
-          createElement('th', {}, 'End', [])
-        ])
-      ]),
-      createElement('tbody', {id: 'track-form-container'}, '', [])
-    ]),
-    createElement('input', {id: 'track-form-submit-button', type: 'submit', value: 'Submit Tracks'}, '', [])
+  const $formTable =
+  createElement('row', {}, '', [
+    createElement('div', {class: 'col-md-8 col-md-offset-2'}, '', [
+      createElement('form', {id: 'tracklist-form'}, '', [
+        createElement('table', {class: 'table table-bordered'}, '', [
+          createElement('thead', {}, '', [
+            createElement('tr', {}, '', [
+              createElement('th', {}, 'Num', []),
+              createElement('th', {}, 'Name', []),
+              createElement('th', {}, 'Start', []),
+              createElement('th', {}, 'End', [])
+            ])
+          ]),
+          createElement('tbody', {id: 'track-form-container'}, '', [])
+        ]),
+        createElement('input', {id: 'track-form-submit-button', type: 'submit', value: 'Submit Tracks'}, '', [])
+      ])
+    ])
   ])
   return $formTable
 }
