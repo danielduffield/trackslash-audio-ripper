368610330c5293eeb6e62b338031a3abd5d18e24
Daniel Duffield <danieljduffield@gmail.com>
2017-07-13 13:29:25 -0700

    Wrote a function to export that takes a track, parses the input time codes, and slices the track to a new file. #2

---
+++ sliceTrack.js
@@ -0,0 +1,41 @@
+const ffmpeg = require('fluent-ffmpeg')
+const path = require('path')
+
+function sliceTrack(track, keyData) {
+  ffmpeg(path.join(__dirname, '/' + track.trackName + '.mp3'))
+    .setStartTime(track.startTime)
+    .setDuration(calculateDuration(track))
+    .output(path.join(__dirname, track.trackName + '.mp3'))
+    .on('end', function (err, data) {
+      if (!err) {
+        console.log('conversion Done')
+      }
+    })
+    .on('error', function (err) {
+      console.log(new Error(err))
+
+    }).run()
+}
+
+function calculateDuration(track) {
+  const parsedStart = parseTime(track.trackStart)
+  const parsedEnd = parseTime(track.trackEnd)
+  const parsedDuration = {}
+  parsedDuration.hours = parsedEnd.hours - parsedStart.hours
+  parsedDuration.minutes = parsedEnd.minutes - parsedStart.minutes
+  if (parsedDuration.minutes < 0) parsedDuration.minutes += 60
+  parsedDuration.seconds = parsedEnd.seconds - parsedStart.seconds
+  if (parsedDuration.seconds < 0) parsedDuration.seconds += 60
+  const duration = (parsedDuration.hours * 3600) + (parsedDuration.minutes * 60) + parsedDuration.seconds
+  return duration
+}
+
+function parseTime(time) {
+  const parsedTime = {}
+  parsedTime.hours = parseInt(time[0] + time[1], 10)
+  parsedTime.minutes = parseInt(time[3] + time[4], 10)
+  parsedTime.seconds = parseInt(time[6] + time[7], 10)
+  return parsedTime
+}
+
+module.exports = sliceTrack
