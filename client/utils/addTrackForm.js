b5745b9151cbb02009f7593ce7516ce17f582be2
Daniel Duffield <danieljduffield@gmail.com>
2017-07-17 13:25:39 -0700

    Moved createTrackForm function to the addTrackForm file to clean up file structure. #3

--- client/utils/addTrackForm.js
+++ client/utils/addTrackForm.js
@@ -1,8 +1,23 @@
-const createTrackForm = require('./createTrackForm.js')
+const createElement = require('./createElement')
 
 function addTrackForm(currentTrack) {
   const $trackFormContainer = document.getElementById('track-form-container')
   $trackFormContainer.appendChild(createTrackForm(currentTrack))
+}
+
+function createTrackForm(currentTrack) {
+  const trackFormFields = ['num', 'name', 'start', 'end']
+  const $trackForm = createElement('tr', {class: 'track-form-' + currentTrack}, '', [])
+  for (let i = 0; i < trackFormFields.length; i++) {
+    const $tableCell = createElement('td', {}, '', [])
+    const $trackFormField = createElement('input', {
+      id: 'track-' + trackFormFields[i] + '-' + currentTrack,
+      name: 'track-' + trackFormFields[i] + '-' + currentTrack
+    }, '', [])
+    $tableCell.appendChild($trackFormField)
+    $trackForm.appendChild($tableCell)
+  }
+  return $trackForm
 }
 
 module.exports = addTrackForm
