2b390a17f0ffaa439eccbac13f6f33da8c168578
Daniel Duffield <danieljduffield@gmail.com>
2017-07-17 11:03:15 -0700

    Removed console logs from resetTracklist.js. #3

--- client/utils/resetTracklist.js
+++ client/utils/resetTracklist.js
@@ -1,17 +1,11 @@
 function resetTracklist() {
-  console.log('Resetting')
   const $trackFormContainer = document.getElementById('track-form-container')
-  console.log('track form container: ', $trackFormContainer)
   let resetIndex = 1
   let $trackForm = document.querySelector('.track-form-' + resetIndex)
-  console.log('first track: ', $trackForm)
   while ($trackForm) {
-    console.log('trackform to be removed: ', $trackForm)
     $trackFormContainer.removeChild($trackForm)
     resetIndex++
-    console.log('reset index: ', resetIndex)
     $trackForm = document.querySelector('.track-form-' + resetIndex)
-    console.log('next trackform to remove: ', $trackForm)
   }
 }
 
