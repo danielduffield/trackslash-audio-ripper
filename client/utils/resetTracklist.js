3acc88253405e2162b134db094f0dddfbc3d2972
Daniel Duffield <danieljduffield@gmail.com>
2017-07-17 10:19:40 -0700

    Fixed a bug where track-form-container was not properly being selected by getElementById. #3

--- client/utils/resetTracklist.js
+++ client/utils/resetTracklist.js
@@ -1,11 +1,17 @@
 function resetTracklist() {
-  const $trackFormContainer = document.getElementById('trackFormContainer')
+  console.log('Resetting')
+  const $trackFormContainer = document.getElementById('track-form-container')
+  console.log('track form container: ', $trackFormContainer)
   let resetIndex = 1
   let $trackForm = document.querySelector('.track-form-' + resetIndex)
+  console.log('first track: ', $trackForm)
   while ($trackForm) {
+    console.log('trackform to be removed: ', $trackForm)
     $trackFormContainer.removeChild($trackForm)
     resetIndex++
+    console.log('reset index: ', resetIndex)
     $trackForm = document.querySelector('.track-form-' + resetIndex)
+    console.log('next trackform to remove: ', $trackForm)
   }
 }
 
