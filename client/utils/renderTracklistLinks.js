a3736e13819f650220e40c25cfdfdbf8ec106949
Daniel Duffield <danieljduffield@gmail.com>
2017-07-18 10:58:54 -0700

    Removed lines adding th to tracklist form table in anticipation for creating new tracklist table. #4

--- client/utils/renderTracklistLinks.js
+++ client/utils/renderTracklistLinks.js
@@ -1,12 +1,6 @@
-const createElement = require('./createElement')
-
 function renderTracklistLinks($tracklist) {
-  const $theadRow = document.querySelector('.thead-row')
-  const $downloadTH = createElement('th', {}, 'DL', [])
-  $theadRow.appendChild($downloadTH)
-
   $tracklist.forEach(($trackLink, index) => {
-    const $trackForm = document.querySelector('.track-form-' + (index + 1))
+    const $trackForm = document.querySelector('.track-final-' + (index + 1))
     console.log('index: ', index)
     console.log('trackform: ', $trackForm)
     $trackForm.appendChild($trackLink)
