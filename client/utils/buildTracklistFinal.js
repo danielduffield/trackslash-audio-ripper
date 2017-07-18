356b66c36d97a8f83a7223bc0c1a55470297681e
Daniel Duffield <danieljduffield@gmail.com>
2017-07-18 11:47:34 -0700

    Moved text content of tracklist from td elements to spans. #4

--- client/utils/buildTracklistFinal.js
+++ client/utils/buildTracklistFinal.js
@@ -8,11 +8,11 @@ function buildTracklistFinal(tracklist) {
   tracklist.forEach(track => {
     const $trackFinal = createElement('tr', {class: 'track-final-' + trackIndex}, '', [])
     trackFields.forEach((field, index) => {
-      const $tableCell = createElement('td', {}, track[trackProperties[index]], [])
+      const $tableCell = createElement('td', {}, '', [])
       const $trackFinalField = createElement('span', {
         id: 'track-final-' + trackFields[index] + '-' + trackIndex,
         class: 'track-final-field'
-      }, '', [])
+      }, track[trackProperties[index]], [])
       $tableCell.appendChild($trackFinalField)
       $trackFinal.appendChild($tableCell)
     })
