6ecc31bb036761597c8e5a142698ba4e14273cb0
Daniel Duffield <danieljduffield@gmail.com>
2017-07-17 19:09:19 -0700

    Created a set of functions that take a tracklist and return an array of links to corresponding files. #4

---
+++ client/utils/renderTracklistLinks.js
@@ -0,0 +1,28 @@
+const createElement = require('./createElement.js')
+const path = require('path')
+
+function parseTrackName(track) {
+  const parsedName = track.trackName.split(' ').join('-')
+  return parsedName
+}
+
+function renderTrackLink(track, index, videoId) {
+  const trackPath = path.join(__dirname, '/../../server/downloaded/' + videoId)
+  const fileName = parseTrackName(track)
+  const $trackLink = createElement('a', {
+    class: 'track-link',
+    id: 'track-link-' + (index + 1),
+    href: trackPath + '/' + fileName + '.mp3'
+  }, '+', [])
+  return $trackLink
+}
+
+function renderTracklistLinks(tracklist, videoId) {
+  const $tracklistLinks = []
+  tracklist.forEach((track, index) => {
+    $tracklistLinks.push(renderTrackLink(track, index, videoId))
+  })
+  return $tracklistLinks
+}
+
+module.exports = renderTracklistLinks
