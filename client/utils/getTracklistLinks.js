6d698285d284e1805af585106a45387db4de3c15
Daniel Duffield <danieljduffield@gmail.com>
2017-07-18 09:43:02 -0700

    Changed the file download links to point to the correct path. #4

--- client/utils/getTracklistLinks.js
+++ client/utils/getTracklistLinks.js
@@ -1,5 +1,4 @@
 const createElement = require('./createElement.js')
-const path = require('path')
 
 function parseTrackName(track) {
   const parsedName = track.trackName.split(' ').join('-')
@@ -7,12 +6,13 @@ function parseTrackName(track) {
 }
 
 function createTrackLink(track, index, videoId) {
-  const trackPath = path.join(__dirname, '/../../server/downloaded/' + videoId)
+  const trackPath = '/download/' + videoId
   const fileName = parseTrackName(track)
   const $trackLink = createElement('a', {
     class: 'track-link',
     id: 'track-link-' + (index + 1),
-    href: trackPath + '/' + fileName + '.mp3'
+    href: trackPath + '/' + fileName + '.mp3',
+    download: ''
   }, '+', [])
   return $trackLink
 }
