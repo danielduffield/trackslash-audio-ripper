38e04f3791e7c113b800aeb53e353312d9a9ad57
Daniel Duffield <danieljduffield@gmail.com>
2017-07-20 13:00:58 -0700

    Created a function to delete a track and update all subsequent track numbers. #15

---
+++ client/utils/deleteTrack.js
@@ -0,0 +1,19 @@
+function deleteTrack(trackNumber, totalTracks) {
+  const $container = document.getElementById('track-form-container')
+  const $toBeDeleted = document.querySelector('.track-form-' + trackNumber)
+  $container.removeChild($toBeDeleted)
+
+  const formFields = ['num', 'name', 'start', 'end']
+  for (let i = trackNumber + 1; i <= totalTracks; i++) {
+    const $relabelForm = document.querySelector('.track-form-' + i)
+    $relabelForm.setAttribute('class', 'track-form-' + i)
+
+    formFields.forEach((field, fieldIndex) => {
+      const $form = document.getElementById('track-' + field + '-' + i)
+      $form.setAttribute('id', 'track-' + field + '-' + (i - 1))
+      $form.setAttribute('name', 'track-' + field + '-' + (i - 1))
+    })
+  }
+}
+
+module.exports = deleteTrack
