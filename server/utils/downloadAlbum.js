743317898d836492037b5b432b617c4c8f1eafaa
Daniel Duffield <danieljduffield@gmail.com>
2017-11-17 12:36:53 -0800

    Refactored downloadAlbum to ensure that a zip folder was created. #34

--- server/utils/downloadAlbum.js
+++ server/utils/downloadAlbum.js
@@ -6,34 +6,32 @@ const { io } = require('./serverApp')
 function downloadAlbum(url, keyData, socketId) {
   const albumFolder = path.join(__dirname, '/../downloaded/' + keyData.videoId + '/album')
   const tracksFolder = path.join(__dirname, '/../downloaded/' + keyData.videoId + '/tracks/' + socketId)
+  const zipFolder = path.join(__dirname, '/../downloaded/' + keyData.videoId + '/zip/' + socketId)
+  const downloadFolders = [albumFolder, tracksFolder, zipFolder]
 
   return new Promise((resolve, reject) => {
-    fs.ensureDir(albumFolder)
-      .then(() => {
-        fs.ensureDir(tracksFolder)
+    return Promise.all(downloadFolders.map(dir => fs.ensureDir(dir)))
+    .then(() => {
+      console.log('starting download')
+      const album = ytdl(url, 'audioonly')
+
+      album.pipe(fs.createWriteStream(path.join(albumFolder + '/' + keyData.videoId + '-album.mp3')))
+
+      let previous = null
+      album.on('progress', (chunkLength, downloaded, total) => {
+        console.log(downloaded / total * 100)
+        if (Math.floor(downloaded / total * 100) !== previous) {
+          io.to(socketId).emit('downloadProgress', Math.floor(downloaded / total * 100))
+        }
+        previous = Math.floor(downloaded / total * 100)
       })
-      .then(() => {
-        console.log('starting download')
-        const album = ytdl(url, 'audioonly')
 
-        album.pipe(fs.createWriteStream(path.join(albumFolder + '/' + keyData.videoId + '-album.mp3')))
-
-        let previous = null
-        album.on('progress', (chunkLength, downloaded, total) => {
-          console.log(downloaded / total * 100)
-          if (Math.floor(downloaded / total * 100) !== previous) {
-            io.to(socketId).emit('downloadProgress', Math.floor(downloaded / total * 100))
-          }
-          previous = Math.floor(downloaded / total * 100)
-        })
-
-        album.on('end', () => {
-          console.log('Album done downloading')
-          resolve()
-        })
-        return true
+      album.on('end', () => {
+        console.log('Album done downloading')
+        resolve()
       })
-      .catch(err => console.log(err))
+      return true
+    }).catch(err => console.log(err))
   })
 }
 
