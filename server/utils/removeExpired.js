45ec28369e7aa18b4682ad12234f40019f613f08
Daniel Duffield <danieljduffield@gmail.com>
2017-11-16 19:05:43 -0800

    Used fs-extra fs.remove method to delete expired directories and all contained files. #34

--- server/utils/removeExpired.js
+++ server/utils/removeExpired.js
@@ -7,26 +7,25 @@ const minutes = 1000 * 60
 
 function removeExpired() {
   const expired = findExpired(getBirthtimes(filePath))
+  let filesDeleted = 0
   expired.then(dirInfo => {
-    console.log(dirInfo)
-    /* Promise.all(dirInfo.map(dir => {
+    Promise.all(dirInfo.map(dir => {
       const expiredFilePath = path.join(__dirname, '../downloaded/', dir.fileName)
-      return fs.rmdir(expiredFilePath)
-    })).then(() => console.log('Expired files removed')) */
-
+      console.log('DELETING FILE: ', expiredFilePath)
+      filesDeleted++
+      return fs.remove(expiredFilePath)
+    })).then(() => console.log(filesDeleted + ' Expired directories deleted.'))
   })
 }
 
 function findExpired(fileStats) {
   return fileStats.then(dataArr => {
-    console.log(dataArr)
     return dataArr.filter(fileData => (Date.now() - fileData.stats.birthtimeMs) / minutes > timeLimit)
   })
 }
 
 function getBirthtimes(dir) {
   return fs.readdir(dir).then((files) => {
-    console.log(files)
     return Promise.all(files.map(fileName => {
       const filePath = path.join(__dirname, '../downloaded/', fileName)
       return fs.stat(filePath)
