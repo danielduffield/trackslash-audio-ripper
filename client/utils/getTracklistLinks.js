9956319c4a72ec138e83fa9fe4e4dca97b9abaee
Daniel Duffield <danieljduffield@gmail.com>
2017-11-17 11:40:52 -0800

    Refactored getTracklistLinks function to conform with new socketId directories within track folders. #34

--- client/utils/getTracklistLinks.js
+++ client/utils/getTracklistLinks.js
@@ -5,14 +5,14 @@ function parseTrackName(track) {
   return parsedName
 }
 
-function createTrackLink(track, index, videoId) {
+function createTrackLink(track, index, videoId, socketId) {
   const trackPath = '/download/' + videoId
   const fileName = parseTrackName(track)
   const $linkTD = createElement('td', {}, '', [])
   const $trackLink = createElement('a', {
     class: 'track-link',
     id: 'track-link-' + (index + 1),
-    href: trackPath + '/tracks/' + fileName + '.mp3',
+    href: trackPath + '/tracks/' + socketId + '/' + fileName + '.mp3',
     download: ''
   }, '+', [])
 
@@ -20,11 +20,11 @@ function createTrackLink(track, index, videoId) {
   return $linkTD
 }
 
-function getTracklistLinks(tracklist, videoId) {
+function getTracklistLinks(tracklist, videoId, socketId) {
   const $tracklistLinks = []
 
   tracklist.forEach((track, index) => {
-    $tracklistLinks.push(createTrackLink(track, index, videoId))
+    $tracklistLinks.push(createTrackLink(track, index, videoId, socketId))
   })
 
   return $tracklistLinks
