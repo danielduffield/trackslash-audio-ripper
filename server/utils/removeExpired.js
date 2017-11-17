9f5960dd94b643f25193b7ec8edaeb57c8737fed
Daniel Duffield <danieljduffield@gmail.com>
2017-11-16 18:58:43 -0800

    Refactored removeExpired so that array of directory stats includes corresponding filenames. #34

--- server/utils/removeExpired.js
+++ server/utils/removeExpired.js
@@ -1,23 +1,37 @@
 const fs = require('fs-extra')
 const path = require('path')
 
+const timeLimit = 10
+const filePath = path.join(__dirname, '../downloaded')
+const minutes = 1000 * 60
+
 function removeExpired() {
-  const filePath = path.join(__dirname, '../downloaded')
-  getBirthtimes(filePath).then(data => {
-    console.log(data)
-    const minutes = 1000 * 60
-    data.forEach(fileTime => {
-      console.log((Date.now() - fileTime.birthtimeMs) / minutes)
-    })
+  const expired = findExpired(getBirthtimes(filePath))
+  expired.then(dirInfo => {
+    console.log(dirInfo)
+    /* Promise.all(dirInfo.map(dir => {
+      const expiredFilePath = path.join(__dirname, '../downloaded/', dir.fileName)
+      return fs.rmdir(expiredFilePath)
+    })).then(() => console.log('Expired files removed')) */
+
+  })
+}
+
+function findExpired(fileStats) {
+  return fileStats.then(dataArr => {
+    console.log(dataArr)
+    return dataArr.filter(fileData => (Date.now() - fileData.stats.birthtimeMs) / minutes > timeLimit)
   })
 }
 
 function getBirthtimes(dir) {
-  return fs.readdir(dir).then(files => {
+  return fs.readdir(dir).then((files) => {
     console.log(files)
     return Promise.all(files.map(fileName => {
       const filePath = path.join(__dirname, '../downloaded/', fileName)
       return fs.stat(filePath)
+    })).then(fileStats => fileStats.map((stats, index) => {
+      return { fileName: files[index], stats: Object.assign({}, stats) }
     }))
   })
 }
