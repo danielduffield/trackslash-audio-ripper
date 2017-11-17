0381a44f8cff9ef21aecfafb896900291024f2d8
Daniel Duffield <danieljduffield@gmail.com>
2017-11-17 12:56:11 -0800

    Removed directory ensures from downloadAlbum to be used as separate module. #34

--- server/utils/downloadAlbum.js
+++ server/utils/downloadAlbum.js
@@ -5,34 +5,27 @@ const { io } = require('./serverApp')
 
 function downloadAlbum(url, keyData, socketId) {
   const albumFolder = path.join(__dirname, '/../downloaded/' + keyData.videoId + '/album')
-  const tracksFolder = path.join(__dirname, '/../downloaded/' + keyData.videoId + '/tracks/' + socketId)
-  const zipFolder = path.join(__dirname, '/../downloaded/' + keyData.videoId + '/zip/' + socketId)
-  const downloadFolders = [albumFolder, tracksFolder, zipFolder]
-
   return new Promise((resolve, reject) => {
-    return Promise.all(downloadFolders.map(dir => fs.ensureDir(dir)))
-    .then(() => {
-      console.log('starting download')
-      const album = ytdl(url, 'audioonly')
+    console.log('starting download')
+    const album = ytdl(url, 'audioonly')
 
-      album.pipe(fs.createWriteStream(path.join(albumFolder + '/' + keyData.videoId + '-album.mp3')))
+    album.pipe(fs.createWriteStream(path.join(albumFolder + '/' + keyData.videoId + '-album.mp3')))
 
-      let previous = null
-      album.on('progress', (chunkLength, downloaded, total) => {
-        console.log(downloaded / total * 100)
-        if (Math.floor(downloaded / total * 100) !== previous) {
-          io.to(socketId).emit('downloadProgress', Math.floor(downloaded / total * 100))
-        }
-        previous = Math.floor(downloaded / total * 100)
-      })
+    let previous = null
+    album.on('progress', (chunkLength, downloaded, total) => {
+      console.log(downloaded / total * 100)
+      if (Math.floor(downloaded / total * 100) !== previous) {
+        io.to(socketId).emit('downloadProgress', Math.floor(downloaded / total * 100))
+      }
+      previous = Math.floor(downloaded / total * 100)
+    })
 
-      album.on('end', () => {
-        console.log('Album done downloading')
-        resolve()
-      })
-      return true
-    }).catch(err => console.log(err))
-  })
+    album.on('end', () => {
+      console.log('Album done downloading')
+      resolve()
+    })
+    return true
+  }).catch(err => console.log(err))
 }
 
 module.exports = downloadAlbum
