146df1ab601a1c5496a296fd3ef914128ed4fb6d
Daniel Duffield <danieljduffield@gmail.com>
2017-11-16 17:00:47 -0800

    Added a function that returns a promise.all of data pertaining to directories and files within the downloaded folder. #34

--- server/utils/scan-downloaded.js
+++ server/utils/scan-downloaded.js
@@ -2,10 +2,20 @@ const fs = require('fs-extra')
 const path = require('path')
 
 function scanDownloaded() {
-  readDownloaded().then(files => console.log(files))
+  readDownloaded().then(data => console.log(data))
 }
 
 function readDownloaded() {
+  return getDirectoryNames().then(files => {
+    console.log(files)
+    return Promise.all(files.map(fileName => {
+      const filePath = path.join(__dirname, '../downloaded/', fileName)
+      return fs.stat(filePath)
+    }))
+  })
+}
+
+function getDirectoryNames() {
   console.log('Scanning start')
   const filePath = path.join(__dirname, '../downloaded')
   console.log(filePath)
