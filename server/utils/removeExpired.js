2339f2ba67196dc53ce5142f1111b3fdb4ae02ba
Daniel Duffield <danieljduffield@gmail.com>
2017-11-16 23:01:12 -0800

    Refactored removeExpired to make it more general, to be used with live queue expired file dispostal. #34

--- server/utils/removeExpired.js
+++ server/utils/removeExpired.js
@@ -1,14 +1,9 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const filePath = path.join(__dirname, '../downloaded')
-const minutes = 1000 * 60
-const timeLimit = 20 * minutes
-
-function removeExpired() {
-  const results = findExpired(getFileStats(filePath))
+function removeExpired(directories) {
   let filesDeleted = 0
-  return results.then(dirInfo => {
+  return directories.then(dirInfo => {
     return Promise.all(dirInfo.expired.map(dir => {
       const expiredFilePath = path.join(__dirname, '../downloaded/', dir.fileName)
       console.log('DELETING FILE: ', expiredFilePath)
@@ -18,26 +13,6 @@ function removeExpired() {
       console.log(filesDeleted + ' expired directories deleted.')
       return dirInfo.active
     })
-  })
-}
-
-function findExpired(fileStats) {
-  return fileStats.then(dataArr => {
-    return {
-      expired: dataArr.filter(fileData => (Date.now() - fileData.stats.birthtimeMs) > timeLimit),
-      active: dataArr.filter(fileData => (Date.now() - fileData.stats.birthtimeMs) < timeLimit)
-    }
-  })
-}
-
-function getFileStats(dir) {
-  return fs.readdir(dir).then((files) => {
-    return Promise.all(files.map(fileName => {
-      const filePath = path.join(__dirname, '../downloaded/', fileName)
-      return fs.stat(filePath)
-    })).then(fileStats => fileStats.map((stats, index) => {
-      return { fileName: files[index], stats: Object.assign({}, stats) }
-    }))
   })
 }
 
