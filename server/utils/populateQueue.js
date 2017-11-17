e9606bbc7696ed976b8695f639b53a168b7c37ca
Daniel Duffield <danieljduffield@gmail.com>
2017-11-17 13:32:47 -0800

    Changed queue log to be more consistent with other logs. #34

--- server/utils/populateQueue.js
+++ server/utils/populateQueue.js
@@ -3,13 +3,13 @@ const timeLimit = 20 * minutes
 
 function populateQueue(active) {
   const queue = {}
-  console.log('ACTIVE: ', active)
   active.forEach(dir => {
     const entry = {}
     entry.expiration = Date.now() + timeLimit
     entry.dl = Promise.resolve(true)
     queue[dir.fileName] = entry
   })
+  console.log('QUEUE: ', queue)
   return queue
 }
 
