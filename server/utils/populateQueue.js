8951acc5e88cc439e47698ef0fbc307188012f7a
Daniel Duffield <danieljduffield@gmail.com>
2017-11-17 10:17:41 -0800

    Refactored populateQueue to return the generated queue object. #34

--- server/utils/populateQueue.js
+++ server/utils/populateQueue.js
@@ -2,16 +2,15 @@ const minutes = 60 * 1000
 const timeLimit = 20 * minutes
 
 function populateQueue(active) {
-  return active.then(dirInfo => {
-    const queue = {}
-    dirInfo.forEach(dir => {
-      const entry = {}
-      entry.expiration = Date.now() + timeLimit
-      entry.dl = Promise.resolve(true)
-      queue[dir.fileName] = entry
-    })
-    return queue
+  const queue = {}
+  console.log('ACTIVE: ', active)
+  active.forEach(dir => {
+    const entry = {}
+    entry.expiration = Date.now() + timeLimit
+    entry.dl = Promise.resolve(true)
+    queue[dir.fileName] = entry
   })
+  return queue
 }
 
 module.exports = populateQueue
