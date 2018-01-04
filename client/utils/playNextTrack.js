20368ce43772d6979a73b4f229ac4ae9d5348051
Daniel Duffield <danieljduffield@gmail.com>
2018-01-04 10:21:19 -0800

    Imported updateSelectedTrack to the playNextTrack module for transitioning to the next track. #47

--- client/utils/playNextTrack.js
+++ client/utils/playNextTrack.js
@@ -1,3 +1,5 @@
+const updateSelectedTrack = require('./updateSelectedTrack.js')
+
 function playNextTrack($audioPlayer, tracklist, currentTrack, videoId, socketId) {
   let nextTrack
   if (!currentTrack) nextTrack = tracklist[1]
@@ -8,6 +10,7 @@ function playNextTrack($audioPlayer, tracklist, currentTrack, videoId, socketId)
       : null)
   }
   if (!nextTrack) return currentTrack
+  updateSelectedTrack(tracklist, nextTrack)
   const trackFileName = nextTrack.trackName.split(' ').join('-')
   const trackPath = '/download/' + videoId + '/tracks/' + socketId + '/' + trackFileName + '.mp3'
   $audioPlayer.pause()
