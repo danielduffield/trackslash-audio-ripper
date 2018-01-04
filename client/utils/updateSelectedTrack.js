853ae2cfb6bf8b86afdd41eb08b2559b9f90acaf
Daniel Duffield <danieljduffield@gmail.com>
2018-01-04 10:22:53 -0800

    Removed debug logs from updateSelectedTrack. #47

--- client/utils/updateSelectedTrack.js
+++ client/utils/updateSelectedTrack.js
@@ -1,5 +1,4 @@
 function updateSelectedTrack(tracklist, selected) {
-  console.log('UPDATING TRACK')
   const selectedIndex = tracklist.findIndex(track => track.trackName === selected.trackName)
   if (selectedIndex === -1) return
   resetSelected(tracklist.length)
@@ -10,10 +9,6 @@ function updateSelectedTrack(tracklist, selected) {
 function resetSelected(tracklistLength) {
   for (let i = 1; i <= tracklistLength; i++) {
     const $track = document.getElementById('track-final-' + i)
-    console.log($track)
-    console.log($track.classList)
-    console.log($track.classList.value)
-    console.log($track.classList.value.includes('selected'))
     if ($track.classList.value.includes('selected')) $track.classList.remove('selected')
   }
 }
