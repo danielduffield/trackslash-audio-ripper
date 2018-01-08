d8f9988305a73ee8ccdbd699f5d5096a82a5d077
Daniel Duffield <danieljduffield@gmail.com>
2018-01-08 14:36:08 -0800

    Created an audio module class to begin transferring audio player add-on logic to separate smart component. #48

---
+++ client/utils/audioModule.js
@@ -0,0 +1,35 @@
+class AudioModule {
+  constructor($player, tracklist) {
+    this.player = $player
+    this.isContinuous = false
+    this.isShuffled = false
+    this.tracklist = tracklist
+    this.shuffled = tracklist
+    this.current = tracklist[0]
+    this.index = 0
+    console.log($player, tracklist)
+  }
+  playNextTrack() {
+    if (!this.isContinuous) return
+    this.isShuffled
+    ? console.log('Playing next track in shuffled')
+    : console.log('Playing next unshuffled track')
+  }
+  shuffleTracklist() {
+    this.shuffled = shuffleArray(this.tracklist)
+    console.log(this.tracklist, this.shuffled)
+  }
+}
+
+function shuffleArray(array) {
+  let shuffled = array.slice()
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
+    let temp = shuffled[i]
+    shuffled[i] = shuffled[j]
+    shuffled[j] = temp
+  }
+  return shuffled
+}
+
+module.exports = AudioModule
