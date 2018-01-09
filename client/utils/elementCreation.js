b2074ff14290ae1ac01cb161290a87bd670e48ea
Daniel Duffield <danieljduffield@gmail.com>
2018-01-09 13:16:18 -0800

    Created Font Awesome elements within spans corresponding to the skip backward and forward audio control buttons. #50

--- client/utils/elementCreation.js
+++ client/utils/elementCreation.js
@@ -19,11 +19,17 @@ const $audioModule = createElement('div', { id: 'audio-module' }, '', [
   createElement('div', {id: 'audio-wrapper'}, '', [
     $audioPlayer,
     createElement('div', { id: 'audio-controls' }, '', [
+      createElement('span', { class: 'audio-btn-container', title: 'Previous Track' }, '', [
+        createElement('i', { class: 'audio-button fa fa-step-backward', id: 'backward-skip' }, '', [])
+      ]),
+      createElement('span', { class: 'audio-btn-container', title: 'Next Track' }, '', [
+        createElement('i', { class: 'audio-button fa fa-step-forward', id: 'forward-skip' }, '', [])
+      ]),
       createElement('span', { class: 'audio-btn-container', title: 'Continuous Playback' }, '', [
-        createElement('i', { class: 'audio-button fa fa-refresh', id: 'continuous-play' }, '', [])
+        createElement('i', { class: 'audio-button toggle fa fa-refresh', id: 'continuous-play' }, '', [])
       ]),
       createElement('span', { class: 'audio-btn-container', title: 'Shuffle Playback' }, '', [
-        createElement('i', { class: 'audio-button fa fa-random', id: 'shuffle-play' }, '', [])
+        createElement('i', { class: 'audio-button toggle fa fa-random', id: 'shuffle-play' }, '', [])
       ])
     ])
   ])
