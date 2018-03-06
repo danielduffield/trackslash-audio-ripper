f6f265e735296e7deb1c6f1710a2e0a7e5af935f
Daniel Duffield <danieljduffield@gmail.com>
2018-03-06 12:50:25 -0800

    Refactored the timecodeForm and tracklistTable to account for createElement changes.

--- client/components/timecodeForm.js
+++ client/components/timecodeForm.js
@@ -1,22 +1,20 @@
 const createElement = require('./../utils/elementCreation')
 
-function createTimecodeForm(videoTitle) {
-  const $timecodeForm =
+const createTimecodeForm = videoTitle => (
   createElement('row', [
-    createElement('div', {class: 'col-md-8 col-md-offset-2 view hidden', id: 'submit-timecodes'}, [
-      createElement('div', {id: 'video-image-timecode-form', class: 'video-image hidden'}),
-      createElement('h3', {id: 'timecode-video-title'}, videoTitle),
-      createElement('div', {class: 'timecode-submit-input'}, [
-        createElement('h5', {id: 'timecode-subtitle', class: 'subtitle'}, 'Copy and Paste Timecodes'),
-        createElement('div', {id: 'timecode-error-message-container', class: 'error-message'}),
-        createElement('textarea', {id: 'timecode-input-box', name: 'timecodes-textarea', cols: '70', rows: '10'}),
-        createElement('br'),
-        createElement('button', {id: 'timecode-submit-button', class: 'form-button'}, 'Submit'),
-        createElement('button', {id: 'timecode-cancel-button', class: 'form-button'}, 'Cancel')
-      ])
-    ])
+    ['div', {class: 'col-md-8 col-md-offset-2 view hidden', id: 'submit-timecodes'}, [
+      ['div', {id: 'video-image-timecode-form', class: 'video-image hidden'}],
+      ['h3', {id: 'timecode-video-title'}, videoTitle],
+      ['div', {class: 'timecode-submit-input'}, [
+        ['h5', {id: 'timecode-subtitle', class: 'subtitle'}, 'Copy and Paste Timecodes'],
+        ['div', {id: 'timecode-error-message-container', class: 'error-message'}],
+        ['textarea', {id: 'timecode-input-box', name: 'timecodes-textarea', cols: '70', rows: '10'}],
+        ['br'],
+        ['button', {id: 'timecode-submit-button', class: 'form-button'}, 'Submit'],
+        ['button', {id: 'timecode-cancel-button', class: 'form-button'}, 'Cancel'],
+      ]],
+    ]],
   ])
-  return $timecodeForm
-}
+)
 
 module.exports = createTimecodeForm()
--- client/components/tracklistTable.js
+++ client/components/tracklistTable.js
@@ -4,31 +4,29 @@ const $audioModule = require('./audioModule')
 const columns = ['Num', 'Name', 'Start', 'End', 'DL']
 const $tableColumns = columns.map(col => createElement('th', col))
 
-function createTracklistTable() {
-  const $formTable =
+const createTracklistTable = () => (
   createElement('row', [
-    createElement('div', {class: 'col-md-8 col-md-offset-2 view hidden', id: 'tracklist-download'}, [
-      createElement('div', {id: 'video-image-tracklist-final', class: 'video-image hidden'}),
-      createElement('h3', {id: 'final-album-title'}),
+    ['div', {class: 'col-md-8 col-md-offset-2 view hidden', id: 'tracklist-download'}, [
+      ['div', {id: 'video-image-tracklist-final', class: 'video-image hidden'}],
+      ['h3', {id: 'final-album-title'}],
       $audioModule,
-      createElement('table', {class: 'table table-bordered'}, [
-        createElement('thead', [
-          createElement('tr', {class: 'thead-row'}, [
+      ['table', {class: 'table table-bordered'}, [
+        ['thead', [
+          ['tr', {class: 'thead-row'}, [
             ...$tableColumns
-          ])
-        ]),
-        createElement('tbody', {id: 'track-final-container'})
-      ]),
-      createElement('input', {id: 'start-over-button', class: 'form-button', type: 'button', value: 'Start Over'}),
+          ]]
+        ]],
+        ['tbody', {id: 'track-final-container'}]
+      ]],
+      ['input', {id: 'start-over-button', class: 'form-button', type: 'button', value: 'Start Over'}],
 
-      createElement('span', {id: 'download-all-container'}, [
-        createElement('form', {id: 'download-all-form', method: 'get'}, [
-          createElement('input', {id: 'download-all-button', class: 'form-button', type: 'submit', value: 'Download All'})
-        ])
-      ])
-    ])
+      ['span', {id: 'download-all-container'}, [
+        ['form', {id: 'download-all-form', method: 'get'}, [
+          ['input', {id: 'download-all-button', class: 'form-button', type: 'submit', value: 'Download All'}]
+        ]]
+      ]]
+    ]]
   ])
-  return $formTable
-}
+)
 
 module.exports = createTracklistTable()
