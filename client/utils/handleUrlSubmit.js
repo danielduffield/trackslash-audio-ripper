6e2f3f0d71dd825ceb393c5479e5c4dce81e04ca
Daniel Duffield <danieljduffield@gmail.com>
2018-03-06 11:24:32 -0800

    Fixed a minor rename bug.

--- client/utils/handleUrlSubmit.js
+++ client/utils/handleUrlSubmit.js
@@ -4,7 +4,7 @@ const addTrackForm = require('./addTrackForm.js')
 
 function createAlbumImage(imageLocation, elementId) {
   const albumImage = new Image(512, 288)
-  myImage.src = imageLocation
+  ablumImage.src = imageLocation
   return albumImage
 }
 
