7dd845798114f9bd8590fdf8892d7a5074c29cc9
Daniel Duffield <danieljduffield@gmail.com>
2018-03-06 12:10:37 -0800

    Refactored createElement function.

--- client/utils/elementCreation.js
+++ client/utils/elementCreation.js
@@ -1,13 +1,26 @@
-function createElement(tagName, attributes, content, $children) {
-  const $element = document.createElement(tagName)
-  $element.textContent = content
-  for (const key in attributes) {
-    $element.setAttribute(key, attributes[key])
-  }
-  $children.forEach($child => {
-    $element.appendChild($child)
+function createElement(...args) {
+  const argArr = args.slice()
+  const elType = argArr.shift()
+
+  let textContent = ''
+  let attributes = {}
+  let children = []
+
+  argArr.forEach(trait => {
+    if (typeof trait === 'string') textContent = trait
+    if (typeof trait === 'object') {
+      if (Array.isArray(trait)) children = trait
+      else attributes = trait
+    }
   })
-  return $element
+
+  const $el = document.createElement(elType)
+
+  $el.textContent = textContent
+  for (const key in attributes) $el.setAttribute(key, attributes[key])
+  children.forEach($child => $el.appendChild($child))
+
+  return $el
 }
 
 module.exports = createElement
