edc3633e314848511b2aa39f069a73d215c8e297
Daniel Duffield <danieljduffield@gmail.com>
2018-03-06 13:01:13 -0800

    Refactored getTracklistLinks with new createElement usage.

--- client/utils/getTracklistLinks.js
+++ client/utils/getTracklistLinks.js
@@ -4,23 +4,21 @@ const demo = true
 
 const parseTrackName = track => track.trackName.split(' ').join('-')
 
-function createTrackLink(track, index, videoId, socketId) {
-  const trackPath = '/download/' + videoId
-  const fileName = parseTrackName(track)
-  const $linkTD = createElement('td', { title: demo ? 'File download is currently disabled.' : '' })
-  const $trackLink = createElement('a', {
-    class: `track-link ${demo ? 'disabled' : ''}`,
-    id: `track-link-${index + 1}`,
-    href: demo ? '/' : `${trackpath}/tracks/${socketId}/${fileName}.mp3`,
-    download: '',
-    title: demo ? 'File download is currently disabled.' : ''
-  }, [
-    createElement('i', { class: 'fa fa-download disabled' })
+const createTrackLink = (track, index, videoId, socketId) => (
+  createElement('td', { title: demo ? 'File download is currently disabled.' : '' }, [
+    ['a', {
+        class: `track-link ${demo ? 'disabled' : ''}`,
+        id: `track-link-${index + 1}`,
+        href: demo ? '/' : `${`/download/${videoId}`}/tracks/${socketId}/${parseTrackName(track)}.mp3`,
+        download: '',
+        title: demo ? 'File download is currently disabled.' : ''
+      },
+      [
+        ['i', { class: 'fa fa-download disabled' }]
+      ],
+    ],
   ])
-
-  $linkTD.appendChild($trackLink)
-  return $linkTD
-}
+)
 
 const getTracklistLinks = (tracklist, videoId, socketId) => (
   tracklist.map((track, idx) => createTrackLink(track, idx, videoId, socketId)))
