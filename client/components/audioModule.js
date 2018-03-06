e4281d367ce99dd853afa0c3f88da69e4b9ba790
Daniel Duffield <danieljduffield@gmail.com>
2018-03-06 09:35:58 -0800

    Refactored to reduce repetition.

--- client/components/audioModule.js
+++ client/components/audioModule.js
@@ -2,6 +2,20 @@ const createElement = require('./../utils/elementCreation')
 
 const $audioPlayer = createElement('audio', { id: 'audio-player', controls: '', controlsList: 'nodownload', src: '' }, '', [])
 $audioPlayer.addEventListener('contextmenu', e => e.preventDefault())
+
+const audioButtonData = [
+	{ title: 'Previous Track', icon: 'audio-button fa fa-step-backward', id: 'backward-skip' },
+	{ title: 'Next Track', icon: 'audio-button fa fa-step-forward', id: 'forward-skip' },
+	{ title: 'Continuous Playback', icon: 'audio-button toggle fa fa-refresh', id: 'continuous-play' },
+	{ title: 'Shuffle Playback', icon: 'audio-button toggle fa fa-random', id: 'shuffle-play' },
+]
+
+const $audioButtons = audioButtonData.map(button => (
+	createElement('span', { class: 'audio-btn-container', title: button.title }, '', [
+		createElement('i', { class: button.icon, id: button.id }, '', [])
+	])
+))
+
 const $audioModule = createElement('div', { id: 'audio-module' }, '', [
   createElement('div', { id: 'now-playing-container' }, ' ', [
     createElement('span', { id: 'now-playing' }, '', [])
@@ -9,18 +23,7 @@ const $audioModule = createElement('div', { id: 'audio-module' }, '', [
   createElement('div', {id: 'audio-wrapper'}, '', [
     $audioPlayer,
     createElement('div', { id: 'audio-controls' }, '', [
-      createElement('span', { class: 'audio-btn-container', title: 'Previous Track' }, '', [
-        createElement('i', { class: 'audio-button fa fa-step-backward', id: 'backward-skip' }, '', [])
-      ]),
-      createElement('span', { class: 'audio-btn-container', title: 'Next Track' }, '', [
-        createElement('i', { class: 'audio-button fa fa-step-forward', id: 'forward-skip' }, '', [])
-      ]),
-      createElement('span', { class: 'audio-btn-container', title: 'Continuous Playback' }, '', [
-        createElement('i', { class: 'audio-button toggle fa fa-refresh', id: 'continuous-play' }, '', [])
-      ]),
-      createElement('span', { class: 'audio-btn-container', title: 'Shuffle Playback' }, '', [
-        createElement('i', { class: 'audio-button toggle fa fa-random', id: 'shuffle-play' }, '', [])
-      ])
+      ...$audioButtons
     ])
   ])
 ])
