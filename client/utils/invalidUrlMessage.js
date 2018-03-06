6371461b38e7eecc48af311dbd8d860901120f52
Daniel Duffield <danieljduffield@gmail.com>
2018-03-06 13:03:51 -0800

    Refactored invalidUrlMessage using createElement.

--- client/utils/invalidUrlMessage.js
+++ client/utils/invalidUrlMessage.js
@@ -1,11 +1,10 @@
 const createElement = require('./elementCreation')
 
-function invalidUrlMessage() {
-  const $invalidUrl = createElement('div', {class: 'alert alert-danger', role: 'alert'}, 'Enter a valid Youtube URL', [
-    createElement('span', {class: 'glyphicon glyphicon-exclamation-sign', 'aria-hidden': 'true'}),
-    createElement('span', {class: 'sr-only'}, 'Error:')
-  ])
-  return $invalidUrl
-}
+const invalidUrlMessage = () => (
+  createElement('div', {class: 'alert alert-danger', role: 'alert'}, 'Enter a valid Youtube URL', [
+      ['span', {class: 'glyphicon glyphicon-exclamation-sign', 'aria-hidden': 'true'}],
+      ['span', {class: 'sr-only'}, 'Error:'],
+    ])
+)
 
 module.exports = invalidUrlMessage
