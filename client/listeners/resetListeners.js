b7786c7c28d26ebd9c40e6c8d6a5fa79287b1c4a
Daniel Duffield <danieljduffield@gmail.com>
2018-03-07 12:02:33 -0800

    Refactored resetListeners to break out functions resetTracklist and startOver.

--- client/listeners/resetListeners.js
+++ client/listeners/resetListeners.js
@@ -2,33 +2,37 @@ const { addLoadRef } = require('./../state/elementRefs')
 const state = require('./../state/state.js')
 const addTrackForm = require('./../utils/addTrackForm.js')
 
-function createResetListeners() {
-  const $startOverBtn = addLoadRef('start-over-button')
+function resetTracklist() {
   const $tracklistError = addLoadRef('tracklist-error-message-container')
   const $trackFormContainer = addLoadRef('track-form-container')
+
+  $tracklistError.textContent = ''
+  $trackFormContainer.innerHTML = ''
+  state.currentTrack = 1
+  addTrackForm(state.currentTrack)
+  state.currentTrack += 1
+}
+
+function startOver() {
   const $trackFinalContainer = addLoadRef('track-final-container')
+
+  $trackFinalContainer.innerHTML = ''
+  window.location.hash = ''
+}
+
+function createResetListeners() {
+  const $startOverBtn = addLoadRef('start-over-button')
   const $resetTracklistBtn = addLoadRef('reset-tracklist-button')
 
   const attachStartOverBtnListener = () => (
     $startOverBtn.addEventListener('click', () => {
-      $tracklistError.textContent = ''
-      $trackFormContainer.innerHTML = ''
-      $trackFinalContainer.innerHTML = ''
-      state.currentTrack = 1
-      addTrackForm(state.currentTrack)
-      state.currentTrack += 1
-      window.location.hash = ''
+      resetTracklist()
+      startOver()
     })
   )
 
   const attachResetTracklistListener = () => (
-    $resetTracklistBtn.addEventListener('click', () => {
-      $tracklistError.textContent = ''
-      $trackFormContainer.innerHTML = ''
-      state.currentTrack = 1
-      addTrackForm(state.currentTrack)
-      state.currentTrack += 1
-    })
+    $resetTracklistBtn.addEventListener('click', () => resetTracklist())
   )
 
   return { attachStartOverBtnListener, attachResetTracklistListener }
